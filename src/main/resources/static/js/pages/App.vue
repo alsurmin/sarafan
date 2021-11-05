81ee85bf2656124aa81e315a0b6eb6b3e3c25e24
Aleksey <amsurmin@gmail.com>
2021-11-05 22:33:05 +0300

    Vue Router. Роутинг в браузере.

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -2,37 +2,48 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title>Sarafan</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <span v-if="profile">{{profile.name}}</span>
+      <v-btn flat
+             v-if="profile"
+             :disabled="$route.path ==='/'"
+             @click="showMessages">
+        Messages
+      </v-btn>
+      <v-spacer />
+      <v-btn flat
+             v-if="profile"
+             :disabled="$route.path ==='/profile'"
+             @click="showProfile">
+        {{profile.name}}
+      </v-btn>
       <v-btn v-if="profile" icon href="/logout">
         <logout-icon />
       </v-btn>
     </v-toolbar>
     <v-content>
-      <v-container v-if="!profile">
-        Необходимо авторизоваться через
-        <a href="/login">Google</a>
-      </v-container>
-      <v-container v-if="profile">
-        <messages-list />
-      </v-container>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
 import { mapState, mapMutations } from 'vuex';
-import MessagesList from 'components/messages/MessageList.vue'
 import { addHandler } from 'util/ws'
 import LogoutIcon from 'vue-material-design-icons/Logout.vue';
 
 export default {
   components: {
-    MessagesList,
     LogoutIcon,
   },
   computed: mapState(['profile']),
-  methods: mapMutations(['addMessageMutation', 'updateMessageMutation', 'removeMessageMutation']),
+  methods: {
+    ...mapMutations(['addMessageMutation', 'updateMessageMutation', 'removeMessageMutation']),
+    showMessages() {
+      this.$router.push('/')
+    },
+    showProfile() {
+      this.$router.push('/profile')
+    }
+  },
   created() {
     addHandler(data => {
       if (data.objectType === 'MESSAGE') {
@@ -53,6 +64,11 @@ export default {
         console.error(`Looks like the object type if unknown "${data.objectType}"`)
       }
     })
+  },
+  beforeMount() {
+    if (!this.profile) {
+      this.$router.replace('/auth')
+    }
   }
 }
 </script>
